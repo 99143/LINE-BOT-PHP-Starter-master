6ff2ed49d64cd24e24384d2d0841d1abd79a3e50
99143 <surachet.ch@gmail.com>
2017-07-07 11:48:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,11 +17,13 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+			$userId = $event['source']['userId'];
+			$id = $event['message']['id'];
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+			//	'text' => $text
+				'text' => $id
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
