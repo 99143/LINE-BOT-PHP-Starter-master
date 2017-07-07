2805863d666f0e161851d39eee75c2d296a6a6df
99143 <surachet.ch@gmail.com>
2017-07-07 13:07:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,8 +23,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => $text ,
-				'test' => "test"
-			//	'text' => $id
+				'userid' => $userid"
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
