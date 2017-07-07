826592ffc03f8b9f0b1e0bba2112601084b07972
99143 <surachet.ch@gmail.com>
2017-07-07 10:47:23 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,6 +23,10 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			$messages = [
+				'type' => 'text',
+				'text' => "ด้วยความยินดี"
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
