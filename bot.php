0d1b9e397b54fe69b1e18c8c5343eea39b00233f
99143 <surachet.ch@gmail.com>
2017-07-07 10:23:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'http://fixie:CCbrkV9qQVdAMuy@velodrome.usefixie.com:80';
+$proxyauth = 'username:password';
 $access_token = 'AISYz0a6PRgbuW9MdaTOuhrM6UmQFcJu+BCr+8YtFL+toxtNmT3X2n25LYqVMBVhlLSIx45sZRlXDVnTfJ7Kk53Dvojdgf1IX5NNJAmkmEOcsc7+hfMvYyeRE5lTIwjo0P3sRMfn3tygW6xvVt6U3AdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -37,6 +39,10 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			
 			$result = curl_exec($ch);
 			curl_close($ch);
 
