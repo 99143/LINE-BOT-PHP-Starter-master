e4284e2bc7007777fbbc8200f0c9aeab8311cfb9
99143 <surachet.ch@gmail.com>
2017-07-07 10:51:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,20 +19,16 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages0 = [
+			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-			$messages1 = [
-				'type' => 'text',
-				'text' => "ด้วยความยินดี"
-			];
-
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages0,$message1],
+				'messages' => [$messages0],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
