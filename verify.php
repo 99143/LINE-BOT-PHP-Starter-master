8bffbe310d12850c9d930484213e918b68f322e7
99143 <surachet.ch@gmail.com>
2017-07-07 10:24:26 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'http://fixie:CCbrkV9qQVdAMuy@velodrome.usefixie.com:80';
+$proxyauth = 'username:password';
 $access_token = 'AISYz0a6PRgbuW9MdaTOuhrM6UmQFcJu+BCr+8YtFL+toxtNmT3X2n25LYqVMBVhlLSIx45sZRlXDVnTfJ7Kk53Dvojdgf1IX5NNJAmkmEOcsc7+hfMvYyeRE5lTIwjo0P3sRMfn3tygW6xvVt6U3AdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +11,10 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
 $result = curl_exec($ch);
 curl_close($ch);
 
