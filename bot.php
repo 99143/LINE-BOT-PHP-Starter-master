7e11fdb23bcefd6b9cd88b89a8a7ecce5272befb
99143 <surachet.ch@gmail.com>
2017-07-07 13:19:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text$text$text
+				'text' => $text
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
@@ -53,3 +53,4 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+echo $text;
