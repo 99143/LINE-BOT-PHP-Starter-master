82fd4bffd8b5244c09021ff0cc89882ff94da173
99143 <surachet.ch@gmail.com>
2017-07-07 11:27:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,7 +22,8 @@ if (!is_null($events['events'])) {
 			$messages = [
 			//	{
 				'type' => 'text',
-				'text' => $text "test"
+				'text' => $text
+				'text' => "test"
 			//	},
 			//	{
            		//	"type":"text",
